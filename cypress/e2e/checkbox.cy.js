bf42320b0ea02b10634ece0f9e1276e32b4aabf3
Patrick de Alcantara Strogueia <patrickdealcantara@gmail.com>
2025-02-03 16:20:36 -0400

    checkbox

---
+++ cypress/e2e/checkbox.cy.js
@@ -0,0 +1,22 @@
+describe('checkbox', () => {
+    beforeEach(() => {
+        cy.goHome();
+        cy.login('papito@cyskills.com.br', 'showtime');
+        cy.userLoggedIn();
+        cy.goTo('/checkbox', 'Checkbox');
+    });
+
+    it('deve preencher o campo de área de texto', () => {
+        cy.get('input[value="1"]')
+            .parent()
+            .click();
+        cy.get('label[for="typescript"]').click();
+    });
+
+    it('deve marcar todas as opções', () => {
+        const langs = ['javascript', 'python', 'rust', 'go', 'typescript'];
+        langs.forEach(lang => {
+            cy.get(`label[for="${lang}"]`).click();
+        });
+    });
+});
