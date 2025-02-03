ca28db67f0b1e5288b027ac3b53f3ad5497f1767
Patrick De Alcantara Strogueia <patrick.strogueia@azi.com.br>
2025-02-03 11:37:28 -0400

    formulário de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,12 @@
 describe('login', () => {
   it('deve logar com sucesso', () => {
-    cy.visit('https://playground.cyskills.com.br')
-  })
-})
+    cy.viewport(1920, 1080);
+    cy.visit('https://playground.cyskills.com.br');
+    cy.get('[data-cy="email"]').type('papito@cyskills.com.br');
+    cy.get('[data-cy="password"]').type('showtime');
+    cy.get('[data-cy="login-button"]').click();
+    cy.get('[data-cy="welcome-title"]')
+      .should('be.visible')
+      .and('have.text', 'Boas vindas ao Cypress Playground');
+  });
+});
