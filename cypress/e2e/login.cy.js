947fcf6eb1732807405616248e2920358cca3dd2
Patrick De Alcantara Strogueia <patrick.strogueia@azi.com.br>
2025-02-03 14:37:17 -0400

    custom commands

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,38 +8,36 @@ describe('login', () => {
   })
 
   it('deve logar com sucesso', () => {
-    cy.get('[data-cy="email"]').type('papito@cyskills.com.br');
-    cy.get('[data-cy="password"]').type('showtime');
-    cy.get('[data-cy="login-button"]').click();
+    cy.login('papito@cyskills.com.br', 'showtime');
     cy.get('[data-cy="welcome-title"]')
       .should('be.visible')
       .and('have.text', 'Boas vindas ao Cypress Playground');
   });
 
-  it('não deve logar com sucesso', () => {
-    cy.get('[data-cy="email"]').type('papito@cyskills.com.br');
-    cy.get('[data-cy="password"]').type('abc123');
-    cy.get('[data-cy="login-button"]').click();
-    cy.get('.notice p')
-      .should('be.visible')
-      .and('have.text', 'E-mail ou senha incorretos. Por favor, tente novamente!');
+  it('não deve logar com senha inválida', () => {
+    cy.login('papito@cyskills.com.br', 'abc123');
+    cy.noticeHave('E-mail ou senha incorretos. Por favor, tente novamente!');
   });
 
   it('não deve logar com e-mail não cadastrado', () => {
-    cy.get('[data-cy="email"]').type('404@cyskills.com.br');
-    cy.get('[data-cy="password"]').type('abc123');
-    cy.get('[data-cy="login-button"]').click();
-    cy.get('.notice p')
-      .should('be.visible')
-      .and('have.text', 'E-mail ou senha incorretos. Por favor, tente novamente!');
+    cy.login('404@cyskills.com.br', 'abc123');
+    cy.noticeHave('E-mail ou senha incorretos. Por favor, tente novamente!');
   });
 
   it('não deve logar com e-mail incorreto', () => {
-    cy.get('[data-cy="email"]').type('www.cyskills.com.br');
-    cy.get('[data-cy="password"]').type('abc123');
-    cy.get('[data-cy="login-button"]').click();
-    cy.get('.notice p')
-      .should('be.visible')
-      .and('have.text', 'O formato do e-mail está incorreto. Por favor, verifique e tente novamente!');
+    cy.login('www.cyskills.com.br', 'abc123');
+    cy.noticeHave('O formato do e-mail está incorreto. Por favor, verifique e tente novamente!');
   });
+});
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.get('[data-cy="email"]').type(email);
+  cy.get('[data-cy="password"]').type(password);
+  cy.get('[data-cy="login-button"]').click();
+});
+
+Cypress.Commands.add('noticeHave', (text) => {
+  cy.get('.notice p')
+    .should('be.visible')
+    .and('have.text', text);
 });
