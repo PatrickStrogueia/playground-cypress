a62d2fd7186033d217e8525b1b15ecb30da24dde
Patrick de Alcantara Strogueia <patrickdealcantara@gmail.com>
2025-03-06 21:41:18 -0400

    Condicionais

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -26,4 +26,19 @@ describe('login', () => {
     cy.login('www.cyskills.com.br', 'abc123');
     cy.noticeHave('O formato do e-mail está incorreto. Por favor, verifique e tente novamente!');
   });
+
+  it('não deve logar sem o e-mail', () => {
+    cy.login(null, 'abc12345');
+    cy.noticeHave('Parece que você esqueceu de informar seu e-mail.');
+  });
+
+  it('não deve logar sem a senha', () => {
+    cy.login('papito@cyskills.com.br', null);
+    cy.noticeHave('Por favor, digite sua senha para continuar.');
+  });
+
+  it('não deve logar sem o e-mail e a senha', () => {
+    cy.login(null, null);
+    cy.noticeHave('Parece que você esqueceu de informar seu e-mail.');
+  });
 });
