bed14a7ac79c6822fa586879aa30961e6ca39537
Patrick de Alcantara Strogueia <patrickdealcantara@gmail.com>
2025-06-06 11:13:14 -0400

    Intercepts Parte 2

--- cypress/e2e/cep.cy.js
+++ cypress/e2e/cep.cy.js
@@ -6,16 +6,39 @@ describe('CEP', () => {
     });
 
     it('Deve cadastrar um endereço consumindo a API dos correios', () => {
-        cy.get('input[name="cep"').type('79022080');
+        const address = {
+            cep: "79022080",
+            logradouro: "Rua Alexandre José Lopes Casali",
+            complemento: "Bairro muito bom",
+            unidade: "Não sei",
+            bairro: "Vila Giocondo Orsi",
+            localidade: "Campo Grande",
+            uf: "MS",
+            estado: "Mato Grosso do Sul",
+            regiao: "Centro-Oeste",
+            ibge: "5002704",
+            gia: "",
+            ddd: "67",
+            siafi: "9051"
+        }
+
+        cy.intercept('GET', 'https://viacep.com.br/ws/79022080/json/', {
+            statusCode: 200,
+            body: address
+        }).as('getCep');
+
+        cy.get('input[name="cep"').type(address.cep);
         cy.contains('button', 'Cadastrar').click();
 
+        cy.wait('@getCep');
+
         cy.get('input[name="logradouro"]', { timeout: 7000 })
-            .should('have.value', 'Rua Alexandre José Lopes Casali');
+            .should('have.value', address.logradouro);
 
         cy.get('#cidade', { timeout: 7000 })
-            .should('have.value', 'Campo Grande');
+            .should('have.value', address.localidade);
 
         cy.get('input[placeholder="Estado"]', { timeout: 7000 })
-            .should('have.value', 'MS');
+            .should('have.value', address.uf);
     });
 });
